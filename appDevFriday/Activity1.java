0105ec1e8de5b5aee977420f37cab0c106ddefba
jasmin <jamontenegro@paterostechnologicalcollege.edu.ph>
2025-03-08 00:40:39 +0800

    Added name: jasmin montenegro

--- appDevFriday/Activity1.java
+++ appDevFriday/Activity1.java
@@ -1,8 +1,8 @@
 public class Activity1 {
     public static void main(String[] args) {
-        System.out.println("List of Students:");
+        System.out.println("JASMIN MONTENEGRO - 2J");
    
         // Collaborators: Add your names below using the format:
-        // System.out.println("JASMIN MONTENEGRO - 2J");
+        // System.out.println("Your Full Name - SECTION");
     }
 }
